28738498e1d0f602281f15acaacc216dde0d1e15
LarryKameZhao <larryzhao1994@outlook.com>
2020-01-02 23:42:14 +0800

    eposide component

--- components/classic/eposide/index.js
+++ components/classic/eposide/index.js
@@ -4,14 +4,34 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index: Number
+    index: {
+      type: Number,
+      observer: function(newVal, oldVal, changedPath) {
+        const val = newVal < 10 ? "0" + newVal : newVal;
+        this.setData({
+          _index: val
+        });
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
-  data: {},
-
+  data: {
+    year: 0,
+    month: "",
+    _index: 0
+  },
+  attached() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    this.setData({
+      year,
+      month
+    });
+  },
   /**
    * 组件的方法列表
    */
